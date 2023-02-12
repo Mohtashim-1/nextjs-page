566d17c2a0c5b7e4ada40ce61be97f8f46250f50
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2023-02-12 17:16:11 +0500

    route group

--- app/(Marketing)/about/page.tsx
+++ app/(Marketing)/about/page.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link'
+import Image from 'next/image'
 export default function Home() {
   return (
     <>
       <h1>About</h1>
       <Link href={'/'}>Home</Link>
+      <Image src="/about.jpg" alt="About-image" width="100" height="100" />
     </>
   )
 }
